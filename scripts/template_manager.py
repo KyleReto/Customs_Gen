a8305922fa995f12a9195d695268c82b6921907d
Alkaroth <66634331+Alkaroth@users.noreply.github.com>
2023-03-15 15:43:11 -0500

    Generates TTS deck json (without character card so far)

--- scripts/template_manager.py
+++ scripts/template_manager.py
@@ -314,6 +314,117 @@ def format_common_text(text):
     text = format_color_words(text)
     return text
 
+
+
+deckNum = 1
+
+# Each custom deck holds a grid image of custom cards
+# Since we are using individual images, we need a custom deck
+# for each card generated. This is only a reference; we will still
+# end with just a single deck object
+def tts_add_custom_deck(ttsData, imgPath):
+    # each custom deck requires its own ID
+    global deckNum
+    deckNum = deckNum + 1
+
+    customDeck = {
+          "FaceURL": imgPath,
+          "BackURL": "https://i.imgur.com/igYZhPh.png",
+          "NumWidth": 1,
+          "NumHeight": 1,
+          "BackIsHidden": True,
+          "UniqueBack": False,
+          "Type": 0
+        }
+
+    ttsData["ObjectStates"][0]["CustomDeck"][deckNum] = customDeck
+    return deckNum
+
+
+def tts_add_deckID(ttsData, deckNum):
+    #For reference on why we multiply by 100, read this article
+    #https://clementespeute.com/blog/tabletop-simulator-deck-format/
+    ttsData["ObjectStates"][0]["DeckIDs"].append(deckNum*100)
+
+def tts_add_contained_card(ttsData, deckNum, charName, card):
+    #Note: I am not sure what many of these do. Some of these values
+    # are discarded since card is used in deck as well
+    contained_card = {
+          "Name": "CardCustom",
+          "Transform": {
+            "posX": 0,
+            "posY": 0,
+            "posZ": 0,
+            "rotX": 0,
+            "rotY": 0,
+            "rotZ": 0,
+            "scaleX": 1.25,
+            "scaleY": 1.0,
+            "scaleZ": 1.25
+          },
+          "Nickname": "Block (N)",
+          "Description": charName,
+          "GMNotes": "",
+          "Memo": "15",
+          "ColorDiffuse": {
+            "r": 0.58431375,
+            "g": 0.0,
+            "b": 0.7019608
+          },
+          "LayoutGroupSortIndex": 0,
+          "Value": 0,
+          "Locked": False,
+          "Grid": True,
+          "Snap": True,
+          "IgnoreFoW": False,
+          "MeasureMovement": False,
+          "DragSelectable": True,
+          "Autoraise": True,
+          "Sticky": True,
+          "Tooltip": False,
+          "GridProjection": False,
+          "HideWhenFaceDown": True,
+          "Hands": True,
+          "CardID": 107,
+          "SidewaysCard": False,
+          "CustomDeck": {
+          },
+          "LuaScript": "",
+          "LuaScriptState": "",
+          "XmlUI": ""
+        }
+    nickname = card["card_name"]
+    if card["card_type"] == "Special":
+        nickname += " (S)"
+    elif card["card_type"] == "Ultra":
+        nickname += " (U)"
+    contained_card["Nickname"] = nickname
+    
+    customDeck = ttsData["ObjectStates"][0]["CustomDeck"][deckNum]
+    contained_card["CustomDeck"][deckNum] = customDeck
+    ttsData["ObjectStates"][0]["ContainedObjects"].append(contained_card)
+
+def add_strike_to_tts(ttsData, charName, card, imgPath):
+
+    #Add Custom Decks
+    deckID = tts_add_custom_deck(ttsData, imgPath)
+    
+    for i in range(int(card["copies"])):
+        #Add DeckID
+        tts_add_deckID(ttsData, deckID)
+
+        #Add Contained Objects
+        tts_add_contained_card(ttsData, deckID, charName, card)
+
+
+    return 0
+
+
+def generate_tts_json(ttsData, charName, outputPath):
+    jsonFile = open(outputPath + '/' + charName + 'Deck.json', "w+")
+    jsonFile.write(json.dumps(ttsData))
+    jsonFile.close()
+
 def create_cards(csvPath, templatePath, outputPath):
 
     #TODO allow pointing to Normals Deck
@@ -324,9 +435,7 @@ def create_cards(csvPath, templatePath, outputPath):
     csv_path = csvPath
     with open(csv_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        special_count = 0
-        ultra_count = 0
+
         char_name = "Character"
 
 
@@ -367,26 +476,27 @@ def create_cards(csvPath, templatePath, outputPath):
             card["template_info"] = template_info
             card["config_info"] = config_info
             
-            if row[1] == 'Special':
-                special_count = special_count + 1
-                generate_card(card).save(outputPath + '/' + row[0] + '.png')
+            savePath = outputPath + '/' + row[0] + '.png'
+            if row[1] == 'Special' or row[1] == 'Ultra':
+                
+                generate_card(card).save(savePath)
+                add_strike_to_tts(ttsData, char_name, card, savePath)
                 print("Generated " + row[0])
-            elif row[1] == 'Ultra':
-                ultra_count = ultra_count + 1
-                generate_card(card).save(outputPath + '/' + row[0] + '.png')
-                print("Generated " + row[0])
+
             elif row[1] == 'Character':
                 char_name = row[0]
-                generate_card(card).save(outputPath + '/' + row[0] + '.png')
+                generate_card(card).save(savePath)
                 print("Generated " + row[0])
             elif row[1] == 'Exceed':
                 card["card_name"] = char_name
-                generate_card(card).save(outputPath + '/' + char_name + '_Exceed.png')
+                savePath = outputPath + '/' + char_name + '_Exceed.png'
+                generate_card(card).save(savePath)
                 print("Generated " + row[0])
             
 
 
             # End front code
+    generate_tts_json(ttsData, char_name, outputPath)
 
             
 
