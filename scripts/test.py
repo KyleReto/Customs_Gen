7ca2545a31b19a3aa7fc7d29645f5c7c5553b4ca
Alkaroth <66634331+Alkaroth@users.noreply.github.com>
2023-04-12 19:22:22 -0500

    Added images for Seventh Cross. Config needs revision

--- scripts/test.py
+++ scripts/test.py
@@ -1,12 +1,25 @@
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import Canvas, PhotoImage, Label
+from PIL import Image, ImageTk
 
-ROOT = tk.Tk()
+root = tk.Tk()
 
-ROOT.withdraw()
-# the input dialog
-USER_INP = simpledialog.askstring(title="Test",
-                                  prompt="What's your Name?:")
 
-# check it out
-print("Hello", USER_INP)
+
+# creat the root window
+root.geometry("1200x1200")
+root.title("Image on backGround")
+root.resizable(True, True)
+
+
+backGround = Image.open('.\\character_images\special_1.png')
+
+
+getImage = Image.open('.\\templates\seventh_cross\images\special_frame.png')
+
+backGround.paste(getImage, (0,0), getImage)
+pic = ImageTk.PhotoImage(backGround)
+display = Label(root, image=pic)
+display.pack()
+root.mainloop()
+
